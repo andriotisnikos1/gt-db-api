3dd50bed6b4134769c41d5955a94d74838a1f7e7
andriotisnikos1 <67287051+andriotisnikos1@users.noreply.github.com>
2021-07-26 02:07:29 +0300

    le wat ?

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from 'mongoose';
+import 'dotenv';
+import router from './routes/routes.js';
 const app = express();
 app.listen(process.env.PORT || 5000);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser : true, useUnifiedTopology : true });
+
 
 app.get('/', (req, res) => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser : true, useUnifiedTopology : true });
-    res.send('App is running')
+    console.log('app is running')
 })
