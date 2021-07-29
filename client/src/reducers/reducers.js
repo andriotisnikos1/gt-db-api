69337e38a8042826b60178657a0c8cbadbe4a47c
Yololeio <67287051+andriotisnikos1@users.noreply.github.com>
2021-07-29 16:16:24 +0300

    zaaa

--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -1,29 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-const reducers =  (items = [], action) => {
-    switch (action.type) {
-        case 'FETCH_ALL':
-           return action;
-        case 'CREATE':
-            return action;
-=======
 const reducers =  (state = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
            return state; 
         case 'CREATE':
             return state;
->>>>>>> parent of 8334742 (z)
-=======
-const reducers =  (state = [], action) => {
-    switch (action.type) {
-        case 'FETCH_ALL':
-           return state; 
-        case 'CREATE':
-            return state;
->>>>>>> parent of 8334742 (z)
         case 'UPDATE':
             return state;
         default:
