d7ee9be372eab731c305e835441128145664177b
Yololeio <67287051+andriotisnikos1@users.noreply.github.com>
2021-07-30 20:59:09 +0300

    Update fetchData.js

--- api/db-related/fetchData.js
+++ api/db-related/fetchData.js
@@ -3,12 +3,19 @@ import mongoose from 'mongoose';
 import newProductSchema from '../models-schemas/newProduct.js';
 const app = express();
 const db = mongoose.connection;
+var a = [
+    1,2
+]
 
-export const getData = app.get('/', async (req, res) => {
+var b = []
+export const getData = app.get('/', async(req, res) => {
     try {
-        const products = await db.useDb('main').collection('products').listIndexes()
-
-        res.send(products).statusCode(200)
+            await a.forEach(async id => {
+            const products = await db.useDb('main').collection('products').findOne({_id: id})
+            b.push(products)    
+        })
+        res.send(b)
+        
     } catch (err) {
         console.log(err)
         res.sendStatus(500)
