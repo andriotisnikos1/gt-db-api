57f770d662c9d5efcd88df0c2c860f7fad9b54ce
Yololeio <67287051+andriotisnikos1@users.noreply.github.com>
2021-07-31 11:17:14 +0300

    Update new-delivery.js

--- client/src/components/new-delivery.js
+++ client/src/components/new-delivery.js
@@ -1,2 +1,18 @@
-import { AppBar, Typography, Toolbar  } from "@material-ui/core";
+import { Paper, Typography, TextField, Button  } from "@material-ui/core";
+import React, {useState, useEffect} from "react";
+import { useDispatch, useSelector } from 'react-redux'
 import useStyles from './styles'
+
+export const DeliveryForm = () => {
+    const classes = useStyles()
+
+    const [formData, setFormData] = useState({
+        _id: '',
+        quantity: ''
+
+    })
+
+    return(
+
+    )
+}
