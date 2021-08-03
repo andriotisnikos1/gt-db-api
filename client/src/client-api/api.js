e00f71c27d064808a10f46a7138217ce6b31c668
andriotisnikos1 <67287051+andriotisnikos1@users.noreply.github.com>
2021-08-03 22:41:21 +0300

    updates

--- client/src/client-api/api.js
+++ client/src/client-api/api.js
@@ -5,19 +5,66 @@ dotenv.config();
 
 const url = process.env.CONNECTION_URL
 
-export const getItems = (id) => {
+export const getItem = async(i) => {
+    const id = Number(i)
     try{
-        axios.get(`${url}/${id}`)
+        await axios.get(`${url}/${id}`)
     } catch(err) {
         console.log(err);
     }
     
 }
 
-export const editItem = (id, quantity) => {
+export const editItem = async(i, q) => {
+    const quantity = Number(q)
+    const id = Number(i)
     try {
-        axios.post(`${url}/${id}/${quantity}`)
+        await axios.post(`${url}/${id}/${quantity}`)
     } catch (err) {
         console.log(err);
     }
+}
+
+export const getAll = async() => {
+    try {
+        await axios.get(`${url}/all/true`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateQuantityAdd = async(i, q) => {
+    const quantity = Number(q)
+    const id = Number(i)
+    try {
+        await axios.patch(`${url}/${id}/${quantity}/true`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateQuantityRemove = async(i, q) => {
+    const quantity = Number(q)
+    const id = Number(i)
+    try {
+        await axios.patch(`${url}/${id}/${quantity}/false`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const createItem = async(i, q, nam) => {
+    const id = Number(i)
+    const name = String(nam)
+    const quantity = Number(q)
+    try {
+        await axios.post(`${url}/${id}/${quantity}/${name}`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteItem = async(i) => {
+    const id = Number(i)
+    await axios.delete(`${url}/${id}`)
 }
