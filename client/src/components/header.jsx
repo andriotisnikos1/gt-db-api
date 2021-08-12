ecb9951902d5b434c1e78ae4f945e4406fafb505
andriotisnikos1 <67287051+andriotisnikos1@users.noreply.github.com>
2021-08-12 23:54:55 +0300

    fml

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Button  } from "@material-ui/core" ;
 import useStyles from './styles'
 import image from '../images/icon.ico'
+
 export const Header = () => {
     const classes = useStyles()
    return( 
@@ -19,7 +20,7 @@ export const Buttons = () => {
     return(
         <div>
               
-             <Button variant="contained" color="primary" className={classes.tbBtnStyles} disableElevation>ΝΕΑ ΠΑΡΑΛΑΒΗ</Button>
+             <Button variant="contained" color="primary" className={classes.tbBtnStyles} id={`new-delivery`} disableElevation>ΝΕΑ ΠΑΡΑΛΑΒΗ</Button>
              <Button variant="contained" color="secondary" className={classes.tbBtnStyles} disableElevation>ΣΥΝΕΡΓΕΙΟ Α</Button>
              <Button variant="contained" color="secondary" className={classes.tbBtnStyles} disableElevation>ΣΥΝΕΡΓΕΙΟ Κ</Button>
              <Button variant="outlined" className={classes.tbBtnStyles} disabled>Coming Soon</Button>
